0a8febc4c5367fb9bb9a3506723501d85405a397
Milo Yip <miloyip@gmail.com>
2015-07-05 19:20:09 +0800

    Add Marsaglia Polar

---
+++ src/marsagliapolar.cpp
@@ -0,0 +1,33 @@
+#include "test.h"
+#include "lcg.h"
+#include <cassert>
+
+template <class T>
+void marsagliapolar(T* data, size_t count) {
+	assert(count % 2 == 0);
+	static const T twopi = T(2.0 * 3.14159265358979323846);
+
+	LCG r;
+	for (size_t i = 0; i < count; i += 2) {
+		T u, v, s;
+		do {
+			u = r() * 2 - 1;
+			v = r() * 2 - 1;
+			s = u * u + v * v;
+		} while (s >= 1 || s == 0);
+
+		T mul = std::sqrt(-2 * std::log(s) / s);
+		data[i    ] = mul * u;
+		data[i + 1] = mul * v;
+	}
+}
+
+static void normaldistf_marsagliapolar(float* data, size_t count) {
+    marsagliapolar(data, count);
+}
+
+static void normaldist_marsagliapolar(double* data, size_t count) {
+    marsagliapolar(data, count);
+}
+
+REGISTER_TEST(marsagliapolar);

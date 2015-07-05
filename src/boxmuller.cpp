39ec872fbbaf941a3e77dd58e14306b7554a2f16
Milo Yip <miloyip@gmail.com>
2015-07-05 19:24:37 +0800

    Prevent log(0) in Box-Muller

--- src/boxmuller.cpp
+++ src/boxmuller.cpp
@@ -1,6 +1,7 @@
 #include "test.h"
 #include "lcg.h"
 #include <cassert>
+#include <limits>
 
 template <class T>
 void boxmuller(T* data, size_t count) {
@@ -9,8 +10,12 @@ void boxmuller(T* data, size_t count) {
 
 	LCG r;
 	for (size_t i = 0; i < count; i += 2) {
-		T u1 = r();
-		T u2 = r();
+        T u1, u2;
+        do {
+            u1 = r();
+            u2 = r();
+        } while (u1 <= std::numeric_limits<T>::min());
+
 		T r = std::sqrt(-2 * std::log(u1));
 		T theta = twopi * u2;
 		data[i    ] = r * std::cos(theta);
